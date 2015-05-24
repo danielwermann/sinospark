93a38a873b86f8105e91bf69ba887e809c911a8a
danielwermann <daniel.wermann@gmail.com>
2015-05-24 14:45:58 -0300

    Initial files

---
+++ SinosPark.Web/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SinosPark.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Sinos Park";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "SinosPark";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contato";
+
+            return View();
+        }
+    }
+}
